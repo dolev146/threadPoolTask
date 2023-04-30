877ffbd4c2be95f3be4f298970a152d2e9b65594
dolev_dublon <d.dublon@alphaomega-eng.com>
2023-04-30 11:20:48 +0300

    added check for flags

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
 all:queue coder task stdinExample
 
 coder:queue
-	gcc coder.c ./libCodec.so -o coder
+	gcc coder.c queue.o ./libCodec.so -o coder -Wall -lpthread 
+
+queue:
+	gcc myqueue.c -c -o queue.o -Wall -lpthread
 
 task:
 	gcc basic_main.c ./libCodec.so -o encoder
@@ -10,4 +13,4 @@ stdinExample:
 		gcc stdin_main.c ./libCodec.so -o tester
 
 clean:
-	-rm -f encoder tester 2>/dev/null a.out 
+	-rm -f encoder tester 2>/dev/null a.out  *.o
--- coder.c
+++ coder.c
@@ -6,6 +6,7 @@
 #include <limits.h>
 #include <pthread.h>
 #include "myqueue.h"
+#include "codec.h"
 
 #define SERVERPORT 5003
 #define BUFSIZE 4096
@@ -26,19 +27,36 @@ void *thread_function(void *arg);
 
 int main(int argc, char **argv)
 {
+
+    if (argc != 3)
+    {
+        printf("error not valid amount of  arguments need a key and a flag\n");
+        printf("for example ./coder 12 -e \n");
+        return 0;
+    }
+
+    // check that the flag is valid (e or d)
+    if (strcmp(argv[2], "-e") != 0 && strcmp(argv[2], "-d") != 0)
+    {
+        printf("error not valid flag\n");
+        printf("for example ./coder 12 -e \n");
+        return 0;
+    }
+
+
     // first off we create a bunch of threads
     for (int i = 0; i < THREAD_POOL_SIZE; i++)
     {
         pthread_create(&thread_pool[i], NULL, thread_function, NULL);
     }
-
+    printf("start... \n");
     while (true)
     {
-        printf("Waiting for Input... \n");
-        int *pclient = (int *)malloc(sizeof(int));
-        *pclient = client_socket;
+
+        // int *pclient = (int *)malloc(sizeof(int));
+        // *pclient = client_socket;
         pthread_mutex_lock(&mutex);
-        enqueue(pclient);
+        // enqueue(pclient);
         pthread_cond_signal(&condition_var);
         pthread_mutex_unlock(&mutex);
     }
@@ -67,15 +85,63 @@ void *thread_function(void *arg)
     }
 }
 
-
 void *handle_connection(void *p_client_socket)
 {
     int client_socket = *((int *)p_client_socket);
     free(p_client_socket);
-    char buffer[BUFSIZE] = "hi from server ";
-    recv(client_socket, client_message, 1024, 0);
-    printf("%s", client_message);
-    send(client_socket, buffer, 1024, 0);
-    close(client_socket);
+    // char buffer[BUFSIZE] = "hi from server ";
+    // recv(client_socket, client_message, 1024, 0);
+    // printf("%s", client_message);
+    // send(client_socket, buffer, 1024, 0);
+    // close(client_socket);
     return NULL;
 }
+
+// #include "codec.h"
+// #include <stdio.h>
+// #include <string.h>
+// #include <stdlib.h>
+
+// // cat 1024.txt | ./tester 2
+// // ./tester 2 < 1024.txt > out
+
+// int main(int argc, char *argv[])
+// {
+// 	if (argc != 2)
+// 	{
+// 	    printf("usage: key < file \n");
+// 	    printf("!! data more than 1024 char will be ignored !!\n");
+// 	    return 0;
+// 	}
+
+// 	int key = atoi(argv[1]);
+// 	printf("key is %i \n",key);
+
+// 	char c;
+// 	int counter = 0;
+// 	int dest_size = 1024;
+// 	char data[dest_size];
+
+// 	while ((c = getchar()) != EOF)
+// 	{
+// 	  data[counter] = c;
+// 	  counter++;
+
+// 	  if (counter == 1024){
+// 		encrypt(data,key);
+// 		printf("encripted data: %s\n",data);
+// 		counter = 0;
+// 	  }
+// 	}
+
+// 	if (counter > 0)
+// 	{
+// 		char lastData[counter];
+// 		lastData[0] = '\0';
+// 		strncat(lastData, data, counter);
+// 		encrypt(lastData,key);
+// 		printf("encripted data:\n %s\n",lastData);
+// 	}
+
+// 	return 0;
+// }
